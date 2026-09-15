7f47f66fc3166c773e3b0cc2c6b9d486df802d93
unknown <pedrohenriquef278@gmail.com>
2026-09-15 19:04:59 -0300

    feat:configurando swagger

---
+++ src/main/java/br/com/pferreira/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package br.com.pferreira.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Pedro Ferreira
+ */
+
+@Configuration
+@OpenAPIDefinition(security = @SecurityRequirement(name = "bearerAuth"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
